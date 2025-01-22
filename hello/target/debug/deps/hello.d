7472c56f4fdeffd3ad0204e5a20d211d2c50cfb4
wisacyful <shenzhuohan64@gmail.com>
2025-01-22 22:48:51 +0800

    third commit

---
+++ hello/target/debug/deps/hello.d
@@ -0,0 +1,5 @@
+D:\20900\Desktop\git-demo\hello\target\debug\deps\hello.exe: src/main.rs
+
+D:\20900\Desktop\git-demo\hello\target\debug\deps\hello.d: src/main.rs
+
+src/main.rs:
---
+++ hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+D:\20900\Desktop\git-demo\hello\target\debug\hello.exe: D:\20900\Desktop\git-demo\hello\src\main.rs
